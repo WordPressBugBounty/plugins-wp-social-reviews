e69ae5e0f50a67a72d1bec77196956d63093bb5f
Sean Murphy <sean@iamseanmurphy.com>
2026-04-17 16:32:26 +0000

    4.2.0

---
+++ wp-social-reviews/app/Views/public/chat-templates/elements/fluent-form.php
@@ -0,0 +1,19 @@
+<?php
+defined('ABSPATH') or die;
+
+use WPSocialReviews\Framework\Support\Arr;
+
+if(!defined('FLUENTFORM_VERSION')){
+    return;
+}
+
+$title = Arr::get($settings, 'ff_settings.header_title', __('Contact Us', 'wp-social-reviews'));
+
+echo '<div class="wpsr-fluentform-wrapper">';
+echo '<h3 class="wpsr-fluent-form-title">' . esc_html(apply_filters('wpsocialreviews/ff_title', $title)) . '</h3>';
+foreach ($settings['channels'] as $wpsr_key => $wpsr_channel) {
+    if($wpsr_channel['name'] === 'fluent_forms' && !strpos($wpsr_channel['credential'], 'fluentform_modal')){
+        echo do_shortcode($wpsr_channel['credential']);
+    }
+}
+echo '</div>';
